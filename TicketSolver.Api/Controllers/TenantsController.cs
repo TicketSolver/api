ce483728246e8bd3cbcf5435aeb28bbf0cd38992
Paulo Holanda <paulorfholanda@gmail.com>
2025-04-22 07:26:57 -0300

    🌟 feature: adição de endpoint para Tenants

---
+++ TicketSolver.Api/Controllers/TenantsController.cs
@@ -0,0 +1,35 @@
+using Microsoft.AspNetCore.Mvc;
+using TicketSolver.Api.Models;
+using TicketSolver.Domain.Persistence.Tables.Tenant;
+using TicketSolver.Domain.Services.Tenant;
+
+namespace TicketSolver.Api.Controllers;
+
+public class TenantsController(TenantsService service) : ControllerBase
+{
+    private readonly TenantsService _service = service;
+
+    [HttpGet]
+    [Route("api/tenants")]
+    public async Task<IActionResult> GetTenants()
+    {   
+        var tenants = await _service.GetAllAsync(HttpContext.RequestAborted);
+        return !tenants.Any() ? Ok(ApiResponse.Ok(tenants)) : NotFound();
+    }
+
+    [HttpPost]
+    [Route("api/tenants")]
+    public async Task<IActionResult> CreateTenant([FromBody] Tenants tenant)
+    {
+
+        return Ok(ApiResponse.Ok(await _service.AddTenantAsync(tenant as Tenants, HttpContext.RequestAborted) ?? throw new InvalidOperationException()));
+    }
+    
+    [HttpGet]
+    [Route("api/tenants/{key}")]
+    public async Task<IActionResult> GetTenantByKey(string key)
+    {
+        var tenant = await _service.GetTenantByKeyAsync(key, HttpContext.RequestAborted);
+        return tenant is not null ? Ok(ApiResponse.Ok(tenant)) : NotFound();
+    }
+}
--- TicketSolver.Domain/Repositories/Tenant/TenantsRepository.cs
+++ TicketSolver.Domain/Repositories/Tenant/TenantsRepository.cs
@@ -7,10 +7,27 @@ namespace TicketSolver.Domain.Repositories.Tenant;
 
 public class TenantsRepository(EFContext context) : EFRepositoryBase<Tenants>(context), ITenantsRepository
 {
+    public async Task<IEnumerable<Tenants>> GetAllAsync()
+    {
+        return await GetAll().ToListAsync();
+    }
     public async Task<Tenants?> GetTenantByKeyAsync(string key, CancellationToken cancellationToken)
     {
         return await GetAll()
             .Where(t => t.AdminKey == key || t.PublicKey == key)
             .FirstOrDefaultAsync(cancellationToken);
-    } 
+    }
+
+    public async Task<Tenants?> AddTenantAsync(Tenants tenant, CancellationToken cancellationToken)
+    {
+        await context.Tenants.AddAsync(tenant, cancellationToken);
+        await context.SaveChangesAsync(cancellationToken);
+        return tenant;
+    }
+    
+    public async Task<bool> IsTenantExistsAsync(string key, CancellationToken cancellationToken)
+    {
+        return await GetAll()
+            .AnyAsync(t => t.AdminKey == key || t.PublicKey == key, cancellationToken);
+    }
 }
